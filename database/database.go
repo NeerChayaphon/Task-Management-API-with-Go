0d2404fdd8bc689ce788e1ab9d6808c0d69eb6d4
chayaphon bunyakan <nchayaphon.bun@gmail.com>
2022-01-14 20:53:24 +0700

    Implimenting DB

---
+++ database/database.go
@@ -0,0 +1,13 @@
+package database
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewDatabase() (*gorm.DB, error) {
+	dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	return db, err
+}
--- main.go
+++ main.go
@@ -3,10 +3,20 @@ package main
 import (
 	"net/http"
 
+	"github.com/NeerChayaphon/Task-Management-API-with-Go/database"
+	"github.com/NeerChayaphon/Task-Management-API-with-Go/model"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+	db, err := database.NewDatabase()
+	if err != nil {
+		panic("fail to connect database")
+	}
+
+	db.AutoMigrate(&model.Task{})
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
